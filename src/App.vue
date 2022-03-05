870ff1a6a60c37b2c04ca071c9d2603842ede532
dan <danielalmenar@gmail.com>
2022-03-05 18:04:35 -0300

    added instructions and changed UI

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@ import ParentComponent from '@/components/ParentComponent.vue'
 @import './assets/base.css';
 
 #app {
-  max-width: 800px;
+  max-width: 1400px;
   margin: 0 auto;
   padding: 2rem;
 
--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -1,51 +1,66 @@
 <template>
-<div class="wrapper">
-<div class="header">
-    <h1>Vue {{ '<slot>' }} example</h1>
-    <h3>by <a href="https://danielalmenar.com">Dan Almenar</a></h3>
-    <h3>Click <a href="https://github.com/dan-almenar/vue-slots-example" target="blank">here</a> 
-    to see the complete code of this app.</h3>
-</div>
-<div class="content-block">
-<h2>Normal rendering:</h2>
-<p>(The data displayed in this block is defined in the <span class="parent">ParentComponent</span>,
-    yet this data is subject to be changed by data comming from the <span class="from-parent">
-    FromParentComponent</span>...)</p>
-</div>
-<div class="users-list">
-    <h2>Current users in database:</h2>
-    <div class="user" v-for="user in users" :key="user.name">
-        <ul>
-            <li>{{ user.name }}</li>
-            <p>{{ user.email }}</p>
-            <p>{{ user.password }}</p>
-        </ul>
+    <div class="header">
+        <h1>Vue {{ '<slot>' }} example</h1>
+        <h3>by <a href="https://danielalmenar.com">Dan Almenar</a></h3>
+        <h3>Click <a href="https://github.com/dan-almenar/vue-slots-example" target="blank">here</a> 
+            to see the code.</h3>
     </div>
-    <div class="content-block">
-        <h2>Placing items into child components and receiving data from them:</h2>
-        <p>(The submit button is passed to the <span class="from-parent">FromParentComponent</span> directly from the
-        <span class="parent">ParentComponent</span>
-            and when it is clicked it calls a function defined in the <span class="parent">ParentComponent</span> with data
-            passed by the <span class="from-parent">FromParentComponent</span> via the {{ '<slot>' }} tag.)</p>
-        <FromParentComponent v-slot='{ userName, userEmail, userPassword }'>
-            <button @click="handleUsers(userEmail, userPassword, userName)">Submit</button>
-        </FromParentComponent>
+    <div class="wrapper">
+        <div class="parent-container">
+
+            <div class="content-block">
+            <h2>Normal rendering:</h2>
+            <p>(The data displayed in this block is defined in the <span class="parent">ParentComponent</span>,
+                yet this data is subject to be changed by data comming from the <span class="from-parent">
+                FromParentComponent</span>...)</p>
+            </div>
+            <div class="users-list">
+                <h2>Current users in database:</h2>
+                <div class="user" v-for="user in users" :key="user.name">
+                    <ul>
+                        <li>{{ user.name }}</li>
+                        <p>{{ user.email }}</p>
+                        <p>{{ user.password }}</p>
+                    </ul>
+                </div>
+            </div>        
+        </div>
+        <div class="from-parent-container">
+            <div class="content-block">
+                <h2>Placing items into child components and receiving data from them:</h2>
+                <p>(The submit button is passed to the <span class="from-parent">FromParentComponent</span> directly from the
+                <span class="parent">ParentComponent</span>
+                    and when it is clicked it calls a function defined in the <span class="parent">ParentComponent</span> with data
+                    passed by the <span class="from-parent">FromParentComponent</span> via the {{ '<slot>' }} tag.)</p>
+                <FromParentComponent v-slot='{ userName, userEmail, userPassword }'>
+                    <button @click="handleUsers(userEmail, userPassword, userName)">Submit</button>
+                </FromParentComponent>
+            </div>        
+        </div>
+
+            <div class="content-block">
+                <h2>Receiving functions:</h2>
+                <p>(The list of users is getting filtered by a function defined on the <span
+                class="to-parent">ToParentComponent</span> and passed to the <span class="parent">
+                ParentComponent</span> via the {{ '<slot>' }} tag.)</p>
+                <ToParentComponent v-slot="{ loggedUsers }">
+                    <h2>Currently logged users:</h2>
+                    <ul v-for="user in loggedUsers(users)" :key="user.name">
+                        <li>{{ user.name }}</li>
+                        <p> {{ user.email }}</p>
+                    </ul>
+                </ToParentComponent>
+            </div>
     </div>
-    <div class="content-block">
-        <h2>Receiving functions:</h2>
-        <p>(The list of users is getting filtered by a function defined on the <span
-        class="to-parent">ToParentComponent</span> and passed to the <span class="parent">
-        ParentComponent</span> via the {{ '<slot>' }} tag.)</p>
-        <ToParentComponent v-slot="{ loggedUsers }">
-            <h2>Currently logged users:</h2>
-            <ul v-for="user in loggedUsers(users)" :key="user.name">
-                <li>{{ user.name }}</li>
-                <p> {{ user.email }}</p>
-            </ul>
-        </ToParentComponent>
+    <div class="instructions">
+        <h3>Instructions:</h3>
+        <p>Use the form to either add a new user
+            by providing a name, email and password
+            different than the ones listed in the
+            'Current users in database' list or to log in
+            one of said users.
+        </p>
     </div>
-</div>
-</div>
 </template>
 
 <script setup>
@@ -115,11 +130,22 @@ const handleUsers = (email, password, name) => {
 
 <style scoped>
 .wrapper {
-    max-width: 600px;
-    margin: 50px auto;
+    width: 100%;
+    margin: 30px auto;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 200px;
+}
+.header {
+    width: 400px;
+    margin: 0 auto;
+    margin-top: 50px;
 }
 .header >*, a {
     font-weight: bold;
+}
+.header > h3 {
+    margin-top: 15px;
 }
 span {
     font-size: 1.3rem;
@@ -158,5 +184,17 @@ button {
     border-radius: 8px;
     font-size: 1.2rem;
     color: firebrick;
+}
+.instructions {
+    width: 80%;
+    margin: 50px auto;
+}
+.instructions > h3 {
+    font-size: 2rem;
+    font-weight: bold;
+}
+.instructions > p {
+    font-size: 1.2rem;
+    margin-top: 15px;
 }
 </style>
